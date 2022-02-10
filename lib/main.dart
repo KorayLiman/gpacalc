db66200f9c051bd511136561325b5a29d06625b7
Koray Liman <koraylimancre@gmail.com>
2022-02-10 13:26:45 +0300

    dsfsdf

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'Widgets/MyWidgets.dart';
+import 'constants/app_constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "GPA Calculator",
+      theme: ThemeData(
+          primarySwatch: Constants.mainColor,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: HomePage(),
+    );
+  }
+}
